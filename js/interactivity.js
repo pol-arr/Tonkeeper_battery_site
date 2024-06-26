d4b0a251223ad1b4964ad00b9b0b1d47ff256a42
reinitd <65181690+reinitd@users.noreply.github.com>
2024-06-26 15:06:35 -0700

    refactor and added footer

---
+++ js/interactivity.js
@@ -0,0 +1,22 @@
+const cards = document.querySelectorAll('.card');
+const tiltFactor = 7;
+const reverseTilt = false;
+
+cards.forEach(item => {
+  item.addEventListener('mousemove', e => {
+    const { left, top, width, height } = item.getBoundingClientRect();
+    const centerX = left + width / 2;
+    const centerY = top + height / 2;
+    let angleX = -((e.clientY - centerY) / (height / 2)) * tiltFactor;
+    let angleY = ((e.clientX - centerX) / (width / 2)) * tiltFactor;
+    if (reverseTilt) {
+        angleX = -angleX;
+        angleY = -angleY;
+    }
+    item.style.transform = `perspective(1000px) rotateX(${angleX}deg) rotateY(${angleY}deg)`;
+  });
+
+  item.addEventListener('mouseleave', () => {
+    item.style.transform = "none";
+  });
+});
